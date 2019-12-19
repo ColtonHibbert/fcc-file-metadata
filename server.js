5d847ad8bef578cf56f248a8bc17c023c264f37f
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2019-12-19 00:11:26 -0700

    metadate logging correctly

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 
 var express = require('express');
 var cors = require('cors');
-
+const multer = require('multer');
+let upload = multer({ dest: 'uploads/' })
 // require and use "multer"...
 
 var app = express();
@@ -13,6 +14,20 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res, next) => {
+  console.log(req,"here is req")
+  console.log(req.file, "here is req.file")
+  const fileName = req.file.originalname;
+  const type = req.file.mimetype;
+  const size = req.file.size;
+  console.log(fileName, type, size)
+  res.json({
+    "name": fileName,
+    "type": type,
+    "size": size
+  })
+})
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
